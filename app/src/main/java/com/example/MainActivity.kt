9416c815086254ef5024e83ccf62b76ecdaa6f6f
bolein <bolein95@gmail.com>
2018-08-14 10:10:20 -0500

    Breaking change: removed shortcut methods from Router

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -2,19 +2,22 @@ package com.example
 
 import android.os.Bundle
 import com.controllers.ControllerActivity
+import com.controllers.FragmentTransitions
 import com.example.domain.App
 import com.example.github.auth.AuthController
 import org.greenrobot.eventbus.Subscribe
 
 class MainActivity : ControllerActivity() {
 
+    val container = R.id.container
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setControllerContainer(R.id.container)
+        setControllerContainer(container)
 
         if (savedInstanceState == null) {
-            show(AuthController(), 0, 0)
+            onTransitionEvent(Show(AuthController()))
         }
     }
 
@@ -29,8 +32,41 @@ class MainActivity : ControllerActivity() {
     }
 
     @Subscribe
-    fun onTransitionEvent(transition: Transition) = when (transition) {
-        is Show -> show(transition.ctrl)
-        is Replace -> replace(transition.ctrl)
+    fun onTransitionEvent(transition: Transition) : Boolean = when (transition) {
+        is Show -> make(
+            FragmentTransitions.Show(
+                container,
+                this,
+                transition.enter,
+                transition.exit,
+                transition.ctrl
+            )
+        )
+        is Replace -> make(
+            FragmentTransitions.Replace(
+                container,
+                this,
+                transition.enter,
+                transition.exit,
+                transition.ctrl
+            )
+        )
+        is Back -> make(
+            FragmentTransitions.Back(
+                container,
+                this,
+                transition.enter,
+                transition.exit
+            )
+        )
     }
+
+    override fun onBackPressed() {
+        if (stack.size() > 1 && !stack.peek()!!.onBackPressed()) {
+            onTransitionEvent(Back())
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
--- app/src/main/java/com/example/Transitions.kt
+++ app/src/main/java/com/example/Transitions.kt
@@ -1,8 +1,23 @@
 package com.example
 
+import android.support.annotation.AnimRes
 import com.controllers.Controller
 
 sealed class Transition
 
-data class Show(val ctrl : Controller<*>) : Transition()
-data class Replace(val ctrl : Controller<*>) : Transition()
+data class Show(
+  val ctrl: Controller<*>,
+  @AnimRes val enter: Int = com.controllers.R.anim.slide_in_right,
+  @AnimRes val exit: Int = com.controllers.R.anim.slide_out_left
+) : Transition()
+
+data class Replace(
+  val ctrl: Controller<*>,
+  @AnimRes val enter: Int = com.controllers.R.anim.fade_in_short,
+  @AnimRes val exit: Int = com.controllers.R.anim.fade_out_short
+) : Transition()
+
+data class Back(
+  @AnimRes val enter: Int = com.controllers.R.anim.slide_in_left,
+  @AnimRes val exit: Int = com.controllers.R.anim.slide_out_right
+) : Transition()
